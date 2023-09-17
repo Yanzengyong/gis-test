f137bb6a1a248a5d538acd0d9725848b8fbf2f15
丁海婷 <dinghaiting@dinghaitings-MacBook-Pro.local>
2023-09-17 20:32:25 +0800

    feat:first commit

---
+++ src/views/Overview.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="overview-container">
+    <div id="cesiumContainer" class="fullSize"></div>
+  </div>
+</template>
+  
+<script setup lang='ts'>
+import { ref, onMounted } from 'vue'
+import { Viewer } from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+import fn from './cesiumDemo'
+onMounted(() => {
+  const viewer = new Viewer("cesiumContainer");
+})
+
+//
+const name = ref('qqq')
+
+const clickName = () => {
+  name.value = 'xxaaa'
+}
+</script>
+  @import 'https://sandcastle.cesium.com/templates/bucket.css';
+<style>
+
+</style>
+<style lang="scss" scoped>
+.overview-container {
+  width: 100%;
+  height: 100vh;
+
+  .cesiumContainer {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
